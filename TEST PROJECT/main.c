d12fb3b20af259203069fd031e5bbf7aca26dfb2
Ian Fennie <IF@ws-94-135.haw-1x.haw-hamburg.de>
2016-12-08 10:33:20 +0100

    Initial Commit

---
+++ TEST PROJECT/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TEST PROJECT
+//
+//  Created by Ian Fennie on 08/12/16.
+//  Copyright © 2016 IF. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
